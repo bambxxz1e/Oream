30189063b05f7d3dd756eef834b8a01500aedb80
윤성연 <s2427@e-mirim.hs.kr>
2025-10-25 00:41:16 +0900

    회원가입 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/LoginPage";
 import Intro from "./pages/Intro";
 import Goal1 from "./pages/GoalSetting1";
+import Join from "./pages/JoinPage";
 
 export default function App() {
   return (
@@ -9,7 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Intro />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
         <Route path="/goal1" element={<Goal1 />} />
       </Routes>
     </BrowserRouter>
